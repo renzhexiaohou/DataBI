cf7e1998505a1e34f35a9f5a21783d9b9417de97
Yubo Xiao <xiaoyubocpu@163.com>
2020-02-23 10:48:07 +0800

    Delete global.R

--- global.R
+++
@@ -1,8 +0,0 @@
-# Golbal environment contains information needed in both the ui and server scripts
-
-library(plotly)
-library(shiny)
-
-py <- plot_ly(username="renzhexiaohou", key="nkBfzdgq9Ag7iFS1sSGc", base_url="https://plot.ly")
-
-source("plotlyGraphWidget.R")
